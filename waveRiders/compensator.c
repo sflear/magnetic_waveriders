756784f0612d9553c6099d25d45c23e75a11bdb3
Stephanie Flear <sflear@vt.edu>
2021-01-26 10:16:39 -0500

    First commit 1/26/21

---
+++ waveRiders/compensator.c
@@ -0,0 +1,7 @@
+#include "compensator.h"
+
+void compensator()
+{
+
+
+}
---
+++ waveRiders/main.c
@@ -0,0 +1,22 @@
+
+
+/**
+ * main.c
+ */
+
+// Header files
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+#include <ti/grlib/grlib.h>
+
+int main(void)
+{
+    compensator();
+	return 0;
+}
+
+/**
+void initialize()
+{
+
+}
+*/
