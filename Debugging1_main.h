ce517eea3858a4ee3c76b66c60b2758d73990a96
jonGrabski <37116730+jonGrabski@users.noreply.github.com>
2021-02-10 09:31:49 -0500

    Add files via upload

---
+++ Debugging1_main.h
@@ -0,0 +1,19 @@
+/*
+ * Debugging1_main.h
+ *
+ *  Created on: Jan 31, 2021
+ *      Author: Jon Grabski
+ */
+
+#ifndef DEBUGGING1_MAIN_H_
+#define DEBUGGING1_MAIN_H_
+
+// Sets PWM period.
+#define periodPWM 10000
+
+// Function definitions.
+float getPIDOutput();
+void timerA_isr();
+
+
+#endif /* DEBUGGING1_MAIN_H_ */
